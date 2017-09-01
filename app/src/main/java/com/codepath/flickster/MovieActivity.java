be7d2f1bf65c34d92c1d3ffe670865fcf493bb0f
David Phan <dphan015@ucr.edu>
2017-09-01 01:30:24 -0700

    attached the MovieAdapter to the data source (movies) and adapter view (ListView)

--- app/src/main/java/com/codepath/flickster/MovieActivity.java
+++ app/src/main/java/com/codepath/flickster/MovieActivity.java
@@ -3,7 +3,9 @@ package com.codepath.flickster;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ListView;
 
+import com.codepath.flickster.adapters.MovieAdapter;
 import com.codepath.flickster.models.Movie;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
@@ -19,11 +21,21 @@ import cz.msebera.android.httpclient.Header;
 public class MovieActivity extends AppCompatActivity {
 
     ArrayList<Movie> movies;
+    MovieAdapter movieAdapter;
+    ListView lvItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
+
+        lvItems = (ListView) findViewById(R.id.lvMovies);
+
+        movies = new ArrayList<>();
+
+        movieAdapter = new MovieAdapter(this, movies);
+
+        lvItems.setAdapter(movieAdapter);
 
         String url = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
 
@@ -35,7 +47,8 @@ public class MovieActivity extends AppCompatActivity {
                 JSONArray movieJsonResults = null;
                 try {
                     movieJsonResults = response.getJSONArray("results");
-                    movies = Movie.fromJSONArray(movieJsonResults);
+                    movies.addAll(Movie.fromJSONArray(movieJsonResults));
+                    movieAdapter.notifyDataSetChanged();
                     Log.d("DEBUG", movieJsonResults.toString());
                 } catch (JSONException e) {
                     e.printStackTrace();
