dfbc6859e735ca5993124f8b34b85744dd019c14
David Phan <dphan015@ucr.edu>
2017-09-01 01:20:53 -0700

    prefixed the base url to the poster_path

--- app/src/main/java/com/codepath/flickster/models/Movie.java
+++ app/src/main/java/com/codepath/flickster/models/Movie.java
@@ -17,7 +17,7 @@ public class Movie {
     String overview;
 
     public String getPosterPath() {
-        return posterPath;
+        return String.format("https://image.tmdb.org/t/p/w342/%s",posterPath);
     }
 
     public String getOriginalTitle() {
