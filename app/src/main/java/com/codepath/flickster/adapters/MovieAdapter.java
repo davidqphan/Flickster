0012435c129385d3af5117de0189ec788aa9ca6a
David Phan <dphan015@ucr.edu>
2017-09-03 21:35:08 -0700

    converted to another method of a ViewHolder implementation

--- app/src/main/java/com/codepath/flickster/adapters/MovieAdapter.java
+++ app/src/main/java/com/codepath/flickster/adapters/MovieAdapter.java
@@ -2,78 +2,110 @@ package com.codepath.flickster.adapters;
 
 import android.content.Context;
 import android.content.res.Configuration;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.codepath.flickster.R;
+import com.codepath.flickster.models.Config;
 import com.codepath.flickster.models.Movie;
 import com.squareup.picasso.Picasso;
 
-import java.util.List;
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by dphan on 9/1/17.
  */
 
-public class MovieAdapter extends ArrayAdapter<Movie> {
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
 
-    private class ViewHolder {
-        ImageView ivImage;
-        TextView tvTitle;
-        TextView tvOverview;
+    Config config;
+    Context context;
+    ArrayList<Movie> movies;
 
-        private void populateData(Movie movie) {
-            tvTitle.setText(movie.getOriginalTitle());
-            tvOverview.setText(movie.getOverview());
-            loadImage(movie);
-        }
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        @Nullable @BindView(R.id.ivPosterImg) ImageView ivPoster;
+        @Nullable @BindView(R.id.ivBackdropImg) ImageView ivBackdrop;
+        @BindView(R.id.tvTitle) TextView tvTitle;
+        @BindView(R.id.tvOverview) TextView tvOverview;
 
-        private void loadImage(Movie movie) {
-            int orientation = getContext().getResources().getConfiguration().orientation;
-
-            if (orientation == Configuration.ORIENTATION_PORTRAIT) {
-                Picasso.with(getContext()).load(movie.getPosterPath()).into(ivImage);
-            } else if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
-                Picasso.with(getContext()).load(movie.getBackdropPath()).into(ivImage);
-            }
+        public ViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
         }
     }
 
-    public MovieAdapter(Context context, List<Movie> movies) {
-        super(context, android.R.layout.simple_list_item_1, movies);
+    public MovieAdapter(ArrayList<Movie> movies) {
+        this.movies = movies;
+    }
+
+    public void setConfig(Config config) {
+        this.config = config;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        Movie movie = getItem(position);
-        ViewHolder viewHolder = new ViewHolder();
-        if (canRecycle(convertView)) {
-            viewHolder = (ViewHolder) convertView.getTag();
-        } else {
-            convertView = inflateNewView(viewHolder, parent);
-        }
-
-        viewHolder.populateData(movie);
-
-        return convertView;
+    public int getItemCount() {
+        return movies.size();
     }
 
-    private boolean canRecycle(View view) {
-        return view != null;
-    }
-
-    private View inflateNewView(ViewHolder viewHolder, ViewGroup parent) {
-        LayoutInflater inflater = LayoutInflater.from(getContext());
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
         View view = inflater.inflate(R.layout.item_movie, parent, false);
 
-        viewHolder.ivImage = view.findViewById(R.id.ivMovieImage);
-        viewHolder.tvTitle = view.findViewById(R.id.tvTitle);
-        viewHolder.tvOverview = view.findViewById(R.id.tvOverview);
-        view.setTag(viewHolder);
-        return view;
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        populateData(holder, position);
+    }
+
+    private void populateData(ViewHolder holder, int position) {
+        Movie movie = movies.get(position);
+        holder.tvTitle.setText(movie.getOriginalTitle());
+        holder.tvOverview.setText(movie.getOverview());
+
+        String imgUrl = buildUrl(movie);
+        ImageView imageView = getImageView(holder);
+
+        loadImage(imgUrl, imageView);
+    }
+
+    private void loadImage(String path, ImageView imageView) {
+        Picasso.with(context)
+                .load(path)
+                .placeholder(R.drawable.ic_movie)
+                .into(imageView);
+    }
+
+    private String buildUrl(Movie movie) {
+        if (isPortrait()) {
+            return config.createImageUrl(movie.getPosterPath(), config.getPosterSize());
+        } else {
+            return config.createImageUrl(movie.getBackdropPath(), config.getBackdropSize());
+        }
+    }
+
+    private ImageView getImageView(ViewHolder holder) {
+        if (isPortrait()) {
+            return holder.ivPoster;
+        } else {
+            return holder.ivBackdrop;
+        }
+    }
+
+    private boolean isPortrait() {
+        return context.getResources()
+                .getConfiguration()
+                .orientation == Configuration.ORIENTATION_PORTRAIT;
     }
 }
