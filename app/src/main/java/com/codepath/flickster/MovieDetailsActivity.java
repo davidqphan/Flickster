d210c8ad89a9308b2ee1bfe00eb4f0054db60664
David Phan <dphan015@ucr.edu>
2017-09-10 22:27:57 -0700

    updated gradle

---
+++ app/src/main/java/com/codepath/flickster/MovieDetailsActivity.java
@@ -0,0 +1,8 @@
+package com.codepath.flickster;
+
+/**
+ * Created by dphan on 9/10/17.
+ */
+
+public class MovieDetailsActivity {
+}
---
+++ app/src/main/java/com/codepath/flickster/models/Video.java
@@ -0,0 +1,8 @@
+package com.codepath.flickster.models;
+
+/**
+ * Created by dphan on 9/10/17.
+ */
+
+public class Video {
+}
